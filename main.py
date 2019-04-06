97fd75240be43db9d2c9f6d954c0e216601b244c
Lamp <dmvoicestudios@gmail.com>
2019-04-06 12:17:27 +0200

    implemented basic sort and connect version

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import sys
 
+
+class Point:
+
+    def __init__(self,ind, xarg, yarg):
+        self.ind = ind
+        self.x = xarg
+        self.y = yarg
+    def __str__(self):
+        return "Point({} - ({},{}))".format(self.ind,self.x,self.y)
+
 filename = sys.argv[1]
 
 f = open(filename, "r")
 
-gen = (ln for ln in f)
-
-n = gen.__next__()
+gen = sorted([ln.split(",") for ln in f][1:], key=lambda e: float(e[2][:-2]))
 
 a = []
 b = []
-for ln in gen:
-    ind, x, y = ln.split(",")
-    flx = float(x)
-    if flx < 0:
-        a.append((int(ind), flx, float(y)))
-    else:
-        b.append((int(ind), flx, float(y)))
 
-class Point:
-    def __init__(self, xarg, yarg):
-        self.x = xarg
-        self.y = yarg
+for ln in gen:
+    flx = float(ln[1])
+    if flx < 0:
+        a.append(Point(int(ln[0]),flx,float(ln[2].replace("\n",""))))
+    else:
+        b.append(Point(int(ln[0]), flx, float(ln[2].replace("\n",""))))
+
 
 def ccw(A,B,C):
     return (C.y-A.y) * (B.x-A.x) > (B.y-A.y) * (C.x-A.x)
@@ -29,12 +33,6 @@ def ccw(A,B,C):
 def intersect(A,B,C,D):
     return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
 
-# Write more tests
-print(intersect(Point(1,2),Point(5,4),Point(2,2),Point(2,9)))
-print(intersect(Point(0,0),Point(0,10),Point(-3,-3),Point(2,2)))
-print(intersect(Point(0,0),Point(0,10),Point(5,1),Point(5,55)))
 
-
-
-print(a)
-print(b)
+for i in range(5):
+    print("{} - {}".format(a[i].ind,b[i].ind))
