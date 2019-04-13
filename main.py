a1289154dff6e7569f2a2440166c98cf5c2868ce
Lamp <dmvoicestudios@gmail.com>
2019-04-13 18:33:24 +0200

    added pruning

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ class Point:
         self.ind = ind
         self.x = xarg
         self.y = yarg
+        self.ac = xarg - yarg
+        self.bd = xarg + yarg
 
     def __str__(self):
         return "Point({} - ({},{}))".format(self.ind, self.x, self.y)
@@ -59,19 +61,54 @@ f = open(filename, "r")
 gen = ([ln.split(",") for ln in f][1:])
 
 c = deque(sorted([Point(int(ln[0]), float(ln[1]), float(ln[2].replace("\n", ""))) for ln in gen], key=lambda e: e.y,
-           reverse=True))
+                 reverse=True))
+
 
 def full_graham(cc):
-    ia = cc.copy() #remove this
+    ia = cc.copy()  # remove this
     if len(ia) == 2:
         return ia
+
+    # Do pruning
+    A = None  # (Ax, Ay) which maximizes x - y
+    B = None  # (Bx, Xy) which maximizes x + y
+    C = None  # (Cx, Cy) which minimizes x - y
+    D = None  # (Dx, Dy) which minimizes x + y
+
+    maxA = float("-inf")
+    maxB = float("-inf")
+    minC = float("inf")
+    minD = float("inf")
+
+    for p in ia:
+        if p.ac > maxA:
+            A = p
+            maxA = p.ac
+        if p.bd > maxB:
+            B = p
+            maxB = p.bd
+        if p.ac < minC:
+            C = p
+            minC = p.ac
+        if p.bd < minD:
+            D = p
+            minD = p.bd
+
+    x1 = max(C.x, D.x)
+    x2 = min(A.x, B.x)
+    y1 = max(A.y, D.y)
+    y2 = min(B.y, C.y)
+
+    ia = [p for p in ia if not (x1 < p.x and p.x < x2 and p.y > y1 and p.y < y2)]
+
+    #Pop the minimum
     minp = ia.pop()
 
     ia = sorted(ia, key=lambda e: degrees_between(minp, e))
     hull = deque([minp, ia[0]])
     links = deque([])
 
-    #Main Graham Loop
+    # Main Graham Loop
     for s in ia[1:]:
         while det(hull[-2], hull[-1], s) <= 0:
             hull.pop()
@@ -90,6 +127,7 @@ def full_graham(cc):
 
 rzs = []
 
+#Main loop
 while c:
     ls = full_graham(c)
     if len(ls) == 4:
@@ -101,7 +139,6 @@ while c:
     # Remove links from c
     for lnk in ls:
         c.remove(lnk)
-
 
 end = time.time()
 print("Total time: {}".format(end - start))
